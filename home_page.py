0857004a7ec7b264e56f40c9abc4721f7e81c6fa
Eren Sakarya <53408712+erensakarya@users.noreply.github.com>
2023-12-03 20:22:51 +0300

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -1,8 +1,15 @@
+import pathlib
 from flask import Blueprint, render_template
+import pandas as pd
+
 
 home_page_blueprint = Blueprint("home_page", __name__)
+
+file_path = pathlib.Path("data/endpoints.csv")
+df = pd.read_csv(filepath_or_buffer=file_path, delimiter=",")
 
 
 @home_page_blueprint.route("/")
 def home_page():
-    return render_template(template_name_or_list="home_page.html")
+    return render_template(template_name_or_list="home_page.html",
+                           data=df.to_html(index=False))
