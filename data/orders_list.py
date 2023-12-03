9322d0a7eb377fbe36d864a65ca021faa99e7a1e
Eren Sakarya <53408712+erensakarya@users.noreply.github.com>
2023-12-03 19:54:09 +0300

    Create orders_list.py

---
+++ data/orders_list.py
@@ -0,0 +1,7 @@
+orders_list = [
+    {"name": "pizza", "status": "ready"},
+    {"name": "hamburger", "status": "being_prepared"},
+    {"name": "pasta", "status": "being_cooked"},
+    {"name": "grilled_meat", "status": "burnt"},
+    {"name": "test", "status": "unknown"},
+]
